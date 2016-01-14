881756f0e3a1647b901d6542d7c1d5f8c52b38a0
Bill Prin <waprin@google.com>
2016-01-13 16:27:15 -0800

    Create an index page

--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
