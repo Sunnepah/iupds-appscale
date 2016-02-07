e097d20c63ca750b957214cc6613fd6976f89bba
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-02-07 23:50:09 +0200

    added namespace url names

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'  # Namespacing URL names
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/specifics/5/
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
