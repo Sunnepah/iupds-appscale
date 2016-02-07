e14d56612ff8a2dce50cb4b9e6af6817a808ead4
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-02-07 22:19:05 +0200

    added decorator to model for unicode compatibility

--- polls/models.py
+++ polls/models.py
@@ -13,16 +13,28 @@
 # limitations under the License.
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+import datetime
+from django.utils import timezone
 
-from django.db import models
 
-
+@python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+@python_2_unicode_compatible
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
