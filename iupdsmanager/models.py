885298303463167e81ba306812a393048ae4832c
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-02-20 15:29:36 +0200

    added models for iupdsmanager app

---
+++ iupdsmanager/models.py
@@ -0,0 +1,71 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    uid = models.BigIntegerField(unique=True, null=False)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=None)
+
+    is_cloud_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    CLOUD_ADMIN = 'CA'
+    APPLICATION_OWNER = 'AO'
+    REGULAR_USER = 'RU'
+
+    ADMIN_TYPE = (
+        (CLOUD_ADMIN, 'Cloud Admin'),
+        (APPLICATION_OWNER, 'Application Owner'),
+        (REGULAR_USER, 'Regular User'),
+    )
+
+    # Cloud admins, Application owners, Regular users
+    admin_type = models.CharField(max_length=15,
+                                  choices=ADMIN_TYPE,
+                                  default=REGULAR_USER)
+
+    def is_upperclass(self):
+        return self.admin_type in (self.CLOUD_ADMIN, self.APPLICATION_OWNER, self.REGULAR_USER)
+
+    def __unicode__(self):
+        return self.profile
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
+
+
+class EmailTrack(models.Model):
+    ACTIVATION = 'ACTIVATION'
+    PASSWORD = 'PASSWORD'
+
+    MAIL_TYPE = (
+        (ACTIVATION, 'Freshman'),
+        (PASSWORD, 'Password'),
+    )
+
+    user_uuid = models.UUIDField(null=False)
+    email_sent = models.SmallIntegerField(default=0)
+    mail_type = models.CharField(max_length=2,
+                                 choices=MAIL_TYPE,
+                                 default=ACTIVATION)
+
+    def is_upperclass(self):
+        return self.mail_type in (self.ACTIVATION, self.PASSWORD)
