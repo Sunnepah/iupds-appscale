546f2e2990d02a4c59ce6a907d14ad84e4bf5ee1
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-03-18 01:36:43 +0200

    Added contacts related models

--- iupdsmanager/models.py
+++ iupdsmanager/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    last_login = models.DateTimeField(auto_now=None)
+    last_login = models.DateTimeField(null=True)
 
     is_cloud_admin = models.BooleanField(default=False)
 
@@ -37,7 +37,7 @@ class Profile(models.Model):
     )
 
     # Cloud admins, Application owners, Regular users
-    admin_type = models.CharField(max_length=15,
+    admin_type = models.CharField(max_length=20,
                                   choices=ADMIN_TYPE,
                                   default=REGULAR_USER)
 
@@ -59,15 +59,103 @@ class EmailTrack(models.Model):
     PASSWORD = 'PASSWORD'
 
     MAIL_TYPE = (
-        (ACTIVATION, 'Freshman'),
+        (ACTIVATION, 'Activation'),
         (PASSWORD, 'Password'),
     )
 
     user_uuid = models.UUIDField(null=False)
-    email_sent = models.SmallIntegerField(default=0)
-    mail_type = models.CharField(max_length=2,
+    email_sent = models.BooleanField(default=False)
+    mail_type = models.CharField(max_length=25,
                                  choices=MAIL_TYPE,
                                  default=ACTIVATION)
 
     def is_upperclass(self):
         return self.mail_type in (self.ACTIVATION, self.PASSWORD)
+
+
+class Contact(models.Model):
+    profile = models.ForeignKey(
+            'Profile',
+            on_delete=models.CASCADE,
+        )
+    contact = models.CharField(null=False, max_length=64)
+
+    PHONE = 'PHONE'
+    FAX = 'FAX'
+    EMAIL = 'EMAIL'
+    WWW = 'WWW'
+    SKYPE = 'SKYPE'
+    SKYPE_NO = 'SKYPE_NO'
+    BLOG = 'BLOG'
+    OTHER = 'OTHER'
+
+    CONTACT_SECTION = (
+        (PHONE, 'Phone'),
+        (FAX, 'Fax'),
+        (EMAIL, 'Email'),
+        (WWW, 'www'),
+        (SKYPE, 'Skype'),
+        (SKYPE_NO, 'Skype_no'),
+        (BLOG, 'Blog'),
+        (OTHER, 'Other'),
+    )
+
+    contact_section = models.CharField(max_length=15,
+                                       choices=CONTACT_SECTION, default=OTHER)
+
+    HOME = 'HOME'
+    OFFICE = 'OFFICE'
+    MOBILE = 'MOBILE'
+    VOICE = 'VOICE'
+
+    CONTACT_TYPE = (
+        (HOME, 'Home'),
+        (OFFICE, 'Offcice'),
+        (MOBILE, 'Mobile'),
+        (VOICE, 'Voice'),
+    )
+
+    contact_type = models.CharField(max_length=15, choices=CONTACT_TYPE, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deletion_date = models.DateTimeField(null=True)
+
+    def __unicode__(self):
+        return self.contact
+
+
+class Address(models.Model):
+    profile = models.ForeignKey(
+                'Profile',
+                on_delete=models.CASCADE,
+            )
+
+    street = models.CharField(max_length=64, null=True)
+    city = models.CharField(max_length=25, null=True)
+    post_code = models.CharField(max_length=8, null=False)
+    county = models.CharField(max_length=25, null=True)
+    district = models.CharField(max_length=25, null=True)
+    city_district = models.CharField(max_length=25, null=True)
+    country = models.CharField(max_length=25, null=True)
+    primary = models.BooleanField(null=False, default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deletion_date = models.DateTimeField(null=True)
+
+    def __unicode__(self):
+        return self.address
+
+
+class Graph(models.Model):
+    profile = models.ForeignKey(
+        'Profile',
+        on_delete=models.CASCADE,
+    )
+
+    graph = models.URLField(null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deletion_date = models.DateTimeField(null=True)
