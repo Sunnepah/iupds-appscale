32e9783d37ee8c22f4d904c8b16e73042fed72e0
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-04-03 21:26:01 +0300

    Added request to create graph sql user and creating default graphs

--- static/javascripts/iupdsmanager/services/dashboard.service.js
+++ static/javascripts/iupdsmanager/services/dashboard.service.js
@@ -102,6 +102,19 @@
         function createGraphs() {
 
             $http({
+                url: "/api/v1/graph/user/",
+                dataType: "json",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).success(function(response){
+                console.log(response);
+            }).error(function(error){
+                console.log(error);
+            });
+
+            $http({
                 url: "/api/v1/create_graphs/",
                 dataType: "json",
                 method: "POST",
