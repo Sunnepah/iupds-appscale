d1f734b1113f43ead42906f51b49cab0a4979d78
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-04-08 02:59:38 +0300

    Removed practice apps

--- jobs/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-
-# Create your models here.
-class Jobs(models.Model):
-        name = models.CharField(max_length=50)
-        description = models.TextField(null=True, blank=True)
--- jobs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.conf.urls import patterns
-
-from .views import JobFormView
-
-urlpatterns = [
-    url(r'^job-form/$',
-    login_required(JobFormView.as_view()), name='job_form'),
-]
