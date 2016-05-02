bc348b05d4c2037341a0f3542356454486eb3e8f
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-05-03 02:25:56 +0300

    modified data import to handle new data format in file

--- appscalehelper/importuserprofile.py
+++ appscalehelper/importuserprofile.py
@@ -24,39 +24,53 @@ cursor = mydb.cursor()
 csv_data = csv.reader(open(file_path, 'r'), delimiter=';')
 
 for row in csv_data:
-    # user_client.disable_user(row[0])
-    # user_client.delete_user(row[0])
-    # continue
-    # data in row: id, email, username, name
+    print row
+    exit(1)
+
+    # data in row:
+    # ['user_id', 'name', 'username', 'email','pwd', 'user_type', '0', '0', '18','created_at', 'last_login', '', '\n']
+
+    user_id_old = row[0]
+    fullname = row[1]
+    username = row[2]
+    email = row[3]
+    created_at = row[9]
+
     # check for duplicate
-    query = ("SELECT * FROM iupdsmanager_profile WHERE email = '" + row[1] + "' OR username = '" + row[2] + "'")
+    query = ("SELECT * FROM iupdsmanager_profile WHERE email = '" + email + "' OR username = '" + username + "'")
     cursor.execute(query)
     data = cursor.fetchall()
 
     if len(data) == 0:
 
         try:
-            if user_client.does_user_exist(row[1]):
-                user_client.disable_user(row[1])
-                user_client.delete_user(row[1])
+            if user_client.does_user_exist(email):
+                user_client.disable_user(email)
+                user_client.delete_user(email)
             else:
                 print "User does not exist, proceed "
 
             # call uaserver to create user
-            response = user_client.create_user(row[1], os.urandom(7))
+            response = user_client.create_user(email, os.urandom(7))
             if response:
-                user_data = user_client.get_user_data(row[1])
-                # print user_data
-                user_id = ''
+                user_data = user_client.get_user_data(email)
+                print user_data
+                appscale_user_id = ''
+
                 sql = "INSERT INTO iupdsmanager_profile(uid, user_id_old, email, username, full_name," \
-                      " created_at, is_active, admin_type) " \
-                      "VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (user_id,
-                                                                            str(MySQLdb.escape_string(row[0])),
-                                                                            str(MySQLdb.escape_string(row[1])),
-                                                                            str(MySQLdb.escape_string(row[2])),
-                                                                            str(MySQLdb.escape_string(row[3])),
-                                                                            str(MySQLdb.escape_string(row[4])),
-                                                                            1, 'RU')
+                      " created_at, is_active, admin_type,first_name, last_name, is_admin,updated_at," \
+                      "is_cloud_admin, appscale_user_id) " \
+                      "VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s','%s','%s')" % \
+                      (
+                          user_id_old,
+                          str(MySQLdb.escape_string(user_id_old)),
+                          str(MySQLdb.escape_string(email)),
+                          str(MySQLdb.escape_string(username)),
+                          str(MySQLdb.escape_string(fullname)),
+                          str(MySQLdb.escape_string(created_at)),
+                          1, 'RU', '', '', 0, '0000-00-00 00:00:00', 0, appscale_user_id
+                      )
+
                 # Execute the SQL command
                 cursor.execute(sql)
                 # Commit your changes in the database
@@ -77,6 +91,8 @@ for row in csv_data:
         print 'Possible duplicate, unable to save'
         logging.info("================= Duplicate =================")
         logging.info(data)
+
+    exit(1)
 # close the connection to the database.
 # mydb.commit()
 cursor.close()
