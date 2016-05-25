4d8b38e39535c79df8206cff37d1e9a851e0cff9
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-05-25 03:11:01 +0300

    Added back django admin app

--- iupds/settings.py
+++ iupds/settings.py
@@ -46,7 +46,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
