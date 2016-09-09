32785b82648174169a966958f5bcc08b2b40f1d4
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-09-09 10:59:47 +0300

    Refactored

--- iupds/settings.py
+++ iupds/settings.py
@@ -199,16 +199,16 @@ APPSC_KEY = 'app_secret'
 
 
 # VIRTUOSO SETTINGS
-SPARQL_ENDPOINT = "http://192.168.33.18:8890/sparql"
-SPARQL_AUTH_ENDPOINT = "http://192.168.33.18:8890/sparql-auth"
+SPARQL_ENDPOINT = "http://192.168.33.30:8890/sparql"
+SPARQL_AUTH_ENDPOINT = "http://192.168.33.30:8890/sparql-auth"
 
-VIRTUOSO_HOST = "192.168.33.18"
+VIRTUOSO_HOST = "192.168.33.30"
 VIRTUOSO_PORT = "8890"
 VIRTUOSO_USER = "dba"
 VIRTUOSO_PASSW = "dba"
 
 
-GRAPH_ROOT = 'https://graph.ir.ee/users'
+GRAPH_ROOT = 'https://mypds.me/users'
 # GRAPH_ROOT = 'http://192.168.33.30:9000/users'
 
 NEW_SQL_USER_ENDPOINT = 'http://192.168.33.30:9000/api/v1/user/new/'
@@ -313,9 +313,9 @@ _DEFAULT_SCOPES = []
 
 # TYK ENDPOINTS
 
-TYK_DASHBOARD_API_CREDENTIAL = 'bf4686ad03e54faf749bdc3c6b0c3609'
-TYK_API_ORG_ID = "5749caca0b0c2433cc000001"
-TYK_API_POLICY_ID = "574a163f0b0c2433cc000003"
+TYK_DASHBOARD_API_CREDENTIAL = '285f090d129541a865dd54eff2d6ed19'
+TYK_API_ORG_ID = "577417e90b0c240354000001"
+TYK_API_POLICY_ID = "57741e9f0b0c240354000003"
 
 TYK_GATEWAY_LISTEN_PORT = '8080'
 # TYK_GATEWAY_HOST = 'http://my-tyk-instance.dev'
@@ -327,8 +327,7 @@ TYK_OAUTH_AUTHORIZE_ENDPOINT = TYK_GATEWAY + '/tyk/oauth/authorize-client/'
 # If the user accepts the Client access and has authenticated successfully,
 # your app calls the Tyk REST API OAuth Authorization endpoint (/tyk/oauth/authorize-client/) with the POST parameters
 
-PDS_API_KEY = '5733a58956c02c0af3000001f5e5d3f5ecdc451a554bb18e26a4997b'
-PDS_API_ID = '746974365fe44c917abfd5c00c085542'
+PDS_API_ID = '4818d7eadbb44c66546536015933a3bc'
 PDS_API_NAME = 'PDS API v1'
 
 # https://tyk.io/docs/tyk-api-gateway-v1-9/tyk-rest-api/
