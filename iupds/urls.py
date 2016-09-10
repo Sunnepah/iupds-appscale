149a6bc11aaa3ef29e314b6dc0aed743d32127a0
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-09-11 00:43:13 +0300

    Moved endpoints to specific app urls file

--- iupds/urls.py
+++ iupds/urls.py
@@ -13,34 +13,8 @@
 # limitations under the License.
 
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
-
-from iupdsmanager.views import index, profile, create_user, logout,\
-    create_contact, contact_details, my_contacts, create_graphs,\
-    drop_graphs, create_graph_user, new_user
-
-from pdsoauth.views import application_list, revoke_application, oauth_login, oauth_tyk_notify, oauth_create_client
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index),
-    url(r'^admin/', admin.site.urls),
-    url(r'^iupdsmanager/', include('iupdsmanager.urls')),
-    url(r'^api/v1/profile/$', profile, name='profile'),
-    url(r'^api/v1/contact/$', create_contact, name='create_contact'),
-    url(r'^api/v1/contact/details$', contact_details, name='contact_details'),
-    url(r'^api/v1/mycontacts/$', my_contacts, name='my_contacts'),
-    url(r'^api/v1/create_user/$', create_user, name='create_user'),
-    url(r'^api/v1/user/new/$', csrf_exempt(new_user), name='new_user'),
-    url(r'^api/v1/graph/user/$', create_graph_user, name='create_graph_user'),
-    url(r'^api/v1/drop_graphs/$', drop_graphs, name='drop_graphs'),
-    url(r'^api/v1/create_graphs/$', create_graphs, name='create_graphs'),
-    url(r'^api/v1/auth/logout/$', logout, name='logout'),
-    url(r'^o/', include('iupdsmanager.urls', namespace='oauth2_provider')),
-    url(r'^oauth/login/$', oauth_login, name='oauth_login'),
-    url(r'^oauth/tyk/notify/$', csrf_exempt(oauth_tyk_notify), name='oauth_tyk_notify'),
-    url(r'^oauth/clients/create/$', csrf_exempt(oauth_create_client), name='oauth_create_client_notify'),
-    url(r'^api/v1/user/applications/$', application_list, name='user-applications'),
-    url(r'^api/v1/user/applications/(?P<pk>[0-9])/$', revoke_application, name='revoke-applications')
+    url(r'^/', include('iupdsmanager.urls')),
+    url(r'^/', include('pdsoauth.urls')),
 ]
--- iupdsmanager/urls.py
+++ iupdsmanager/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
-from . import views
+from iupdsmanager.views import index, profile, create_contact, get_contact_details, create_graphs, \
+    drop_graphs, create_graph_user, new_user
 
 urlpatterns = [
     # ex: /iupdsmanager/
-    url(r'^$', views.index, name='index'),
-    url(r'^profiles/$', views.profile, name='profile'),
+    url(r'^$', index, name='index'),
+    url(r'^api/v1/user/profile/$', profile, name='profile'),
+    url(r'^api/v1/user/contact/$', create_contact, name='create_contact'),
+    url(r'^api/v1/user/contact/details$', get_contact_details, name='contact_details'),
+    url(r'^api/v1/user/new/$', csrf_exempt(new_user), name='new_user'),
+    url(r'^api/v1/user/graph/$', create_graph_user, name='create_graph_user'),
+    url(r'^api/v1/user/graph/create/$', create_graphs, name='create_graphs'),
+    url(r'^api/v1/user/graph/drop/$', drop_graphs, name='drop_graphs')
 ]
